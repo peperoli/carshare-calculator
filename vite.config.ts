c7aff32f4fab53c1d391a0acd7ac4f9a893c3df3
peperoli <shagix.ix@gmail.com>
2025-01-21 10:35:58 +0100

    Feat: Add vite PWA plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,15 @@
-import { reactRouter } from "@react-router/dev/vite";
-import autoprefixer from "autoprefixer";
-import tailwindcss from "tailwindcss";
-import { defineConfig } from "vite";
-import tsconfigPaths from "vite-tsconfig-paths";
+import { reactRouter } from '@react-router/dev/vite'
+import autoprefixer from 'autoprefixer'
+import tailwindcss from 'tailwindcss'
+import { defineConfig } from 'vite'
+import tsconfigPaths from 'vite-tsconfig-paths'
+import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig(({ isSsrBuild, command }) => ({
   build: {
     rollupOptions: isSsrBuild
       ? {
-          input: "./server/app.ts",
+          input: './server/app.ts',
         }
       : undefined,
   },
@@ -18,7 +19,7 @@ export default defineConfig(({ isSsrBuild, command }) => ({
     },
   },
   ssr: {
-    noExternal: command === "build" ? true : undefined,
+    noExternal: command === 'build' ? true : undefined,
   },
-  plugins: [reactRouter(), tsconfigPaths()],
-}));
+  plugins: [reactRouter(), tsconfigPaths(), VitePWA({ registerType: 'autoUpdate' })],
+}))
