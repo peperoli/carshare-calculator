3f756ef9f068f7e9bb039899b511634f490e4393
peperoli <shagix.ix@gmail.com>
2025-12-07 23:08:53 +0100

    Remove express server

--- vite.config.ts
+++ vite.config.ts
@@ -5,14 +5,7 @@ import { defineConfig } from 'vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 import { VitePWA } from 'vite-plugin-pwa'
 
-export default defineConfig(({ isSsrBuild, command }) => ({
-  build: {
-    rollupOptions: isSsrBuild
-      ? {
-          input: './server/app.ts',
-        }
-      : undefined,
-  },
+export default defineConfig(({ command }) => ({
   css: {
     postcss: {
       plugins: [tailwindcss, autoprefixer],
